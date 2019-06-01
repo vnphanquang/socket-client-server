7f685ae1ef4fcb623e42de31341ee56cb3c22711
Nhat Pham <nhatphm@dgroup.co>
2019-06-01 15:39:48 +0700

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -6,33 +6,6 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      percent: 0,
-    }
-  }
-
-  onFileChange = (e) => {
-    const file = e.target.files[0];
-    console.log(file);
-    const data = new FormData();
-    data.append('file', file);
-    const request = new XMLHttpRequest();
-    request.upload.addEventListener('progress', (evt) => {
-
-      const pc = (((evt.loaded / evt.total) * 100));
-      this.setState({
-        percent: Math.ceil(pc),
-      })
-    })
-    request.open('POST', 'http://localhost:4000/upload');
-    request.send(data);
-    // Axios.post('http://localhost:4000/upload', data)
-    //   .then(res => {
-    //     console.log(res);
-    //   })
-    //   .catch(error => {
-    //     console.log(error.response);
-    //   })
   }
 
   render() {
@@ -42,13 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <label htmlFor="avatar">
-          <input onChange={this.onFileChange} name="avatar" id="avatar" type="file" />
-        </label>
-        <div style={{ fontSize: '30px' }}>Uploading {this.state.percent} %</div>
-        <video width="320" height="240" controls>
-          <source src="http://localhost:4000/video-stream" type="video/mp4" />
-        </video>
+        HIIH
       </div>
     );
   }
